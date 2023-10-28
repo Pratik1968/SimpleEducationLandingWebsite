3346953ca6de02dfa21c313cb6a7ec16f353d654
Pratik1968 <47908854+Pratik1968@users.noreply.github.com>
2023-10-28 22:48:17 +0530

    added icon and title of navbar

--- SimpleEductionLandingWebsite/src/Navbar/layout.jsx
+++ SimpleEductionLandingWebsite/src/Navbar/layout.jsx
@@ -1,7 +1,16 @@
+import logo from "../assets/4x/logo.png"
 export default function Navbar(){
     return(
-        <div className="w-full h-20 bg-[#F9E0AE]">
-
+        <div className="flex flex-row  items-center w-full h-20 bg-[#F9E0AE]">
+<IconAndTitle/>
         </div>
+    );
+}
+function IconAndTitle(){
+    return(
+    <div className="flex ml-[7.7rem] ">
+        <img className="w-[2rem] h-[2rem] mr-[1.31rem] " src={logo}/>
+    <p className="text-2xl font-bold">Educare</p>
+    </div>
     );
 }
