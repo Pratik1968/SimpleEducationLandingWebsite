739f75006fc803f78e14072308868e2944975d08
Pratik1968 <47908854+Pratik1968@users.noreply.github.com>
2023-10-28 23:14:51 +0530

    added HeroSection folder and function

--- SimpleEductionLandingWebsite/src/App.jsx
+++ SimpleEductionLandingWebsite/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './Navbar/layout'
+import HeroSection from './HeroSection/layout'
 
 function App() {
 
   return (
     <>
    <Navbar/>
+   <HeroSection/>
     </>
   )
 }
