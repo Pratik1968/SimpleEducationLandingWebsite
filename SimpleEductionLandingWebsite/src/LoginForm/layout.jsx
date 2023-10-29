f5c54501e35c0f12e89c4523041ddbe22ed9b8ce
Pratik1968 <47908854+Pratik1968@users.noreply.github.com>
2023-10-29 11:03:20 +0530

    added name input , email input  and subscribe button

--- SimpleEductionLandingWebsite/src/LoginForm/layout.jsx
+++ SimpleEductionLandingWebsite/src/LoginForm/layout.jsx
@@ -1,7 +1,15 @@
 export default function LoginForm(){
     return(
-        <div className="h-[8.75rem] bg-[#F9E0AE]">
-
+        <div className="self-center w-[70%] h-[8.75rem] bg-[#F9E0AE] flex items-center p-10 gap-10">
+<InputBox placeholder={"Your Name"}/>
+<InputBox placeholder={"Your Email Addresses"}/>
+<SubmitButton/>
         </div>
     );
+}
+function InputBox({placeholder}){
+    return <input placeholder={placeholder} className={`h-[3.9375rem] flex-1 text-center text-md font-semibold`}/>;
+}
+function SubmitButton(){
+    return <button className="h-[3.9375rem]  bg-[#C24914] flex-1 text-md font-semibold text-white">Subscribe</button>;
 }
